dd780607b1c2159731bc0f9f2c087ac6a2b474f7
Sofía Millán <sofiaisabellamillan16@gmail.com>
2025-03-24 18:56:29 -0500

    Unit test execution step

--- src/test/java/com/assesment/users/UsersApplicationTests.java
+++ src/test/java/com/assesment/users/UsersApplicationTests.java
@@ -3,7 +3,7 @@ package com.assesment.users;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class UsersApplicationTests {
 
 	@Test
