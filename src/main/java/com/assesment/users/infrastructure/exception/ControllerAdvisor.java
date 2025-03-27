b0bfa8b808afda4c348d55f5d7618267a0cd1530
Sofía Millán <sofiaisabellamillan16@gmail.com>
2025-03-26 21:46:04 -0500

    Adjusting deployment

--- src/main/java/com/assesment/users/infrastructure/exception/ControllerAdvisor.java
+++ src/main/java/com/assesment/users/infrastructure/exception/ControllerAdvisor.java
@@ -3,7 +3,6 @@ package com.assesment.users.infrastructure.exception;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
